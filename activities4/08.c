60ab2b650863817b5ed4d00c0ae354f60cc82ac0
Souza <71847472+souza10v@users.noreply.github.com>
2020-11-28 17:12:21 -0300

    Add files via upload

---
+++ activities4/08.c
@@ -0,0 +1,16 @@
+// -------------------------------------------------------------------------
+// github.com/souza10v
+// souza10vv@gmail.com
+// -------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int graos,i;
+    graos=pow(2,63);
+    printf("O número de grãos é %d ",graos);
+    return 0;
+}
+  
